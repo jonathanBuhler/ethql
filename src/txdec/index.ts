66a2fef5ab21d0e853ab912d652d777d4fe6175b
jonathanBuhler <benedict.buhler@gmail.com>
2018-07-18 16:49:07 +0100

    Initial implementation of ERC721 decoding
    Implementation based on the decoding of ERC20 tokens. Although at the moment it breaks the decoding, but working on that

--- src/txdec/index.ts
+++ src/txdec/index.ts
@@ -1,4 +1,5 @@
 import Erc20Token from './decoders/Erc20Token';
+import ERC721Token from './decoders/erc721Token'
 import { SimpleTxDecodingEngine } from './engines/SimpleTxDecodingEngine';
 import { TxDecodingEngine } from './types';
 
@@ -7,6 +8,7 @@ const engine = new SimpleTxDecodingEngine();
 
 // Register all decoders.
 engine.register(new Erc20Token());
+engine.register(new ERC721Token());
 
 // Export the engine, making only the decodeTransaction method visible.
 export default engine as Pick<TxDecodingEngine, 'decodeTransaction'>;
