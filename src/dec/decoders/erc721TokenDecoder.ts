9f9d50ed88b9060bcf61007a75f96a852a3110c8
jonathanBuhler <benedict.buhler@gmail.com>
2018-07-23 11:59:18 +0100

    Readded files

--- src/dec/decoders/erc721TokenDecoder.ts
+++ src/dec/decoders/erc721TokenDecoder.ts
@@ -60,20 +60,43 @@ interface ERC721setApprovalForAll extends ERC721Transaction {
   approved: boolean;
 }
 
-type ERC721Bindings = {
+type ERC721TransferEvent = {
+  from: ERC721TokenHolder;
+  to: ERC721TokenHolder;
+  tokenID: number;
+};
+
+type ERC721ApprovalEvent = {
+  owner: ERC721TokenHolder;
+  approved: ERC721TokenHolder;
+  tokenID: number;
+};
+
+type ERC721ApprovalForAllEvent = {
+  owner: ERC721TokenHolder;
+  operator: ERC721TokenHolder;
+  approved: boolean;
+};
+
+type ERC721TxBindings = {
   safeTransferFrom: ERC721Transfer;
   transferFrom: ERC721Transfer;
   approve: ERC721Approve;
   setApprovalForAll: ERC721setApprovalForAll;
 };
 
-class ERC721Token {
-  //implements DecoderDefinition<Erc721TxBindings, Erc20LogBindings> {
+type ERC721LogBindings = {
+  transfer: ERC721TransferEvent;
+  approval: ERC721ApprovalEvent;
+  approvalForAll: ERC721ApprovalForAllEvent;
+};
+
+class ERC721TokenDecoder implements DecoderDefinition<ERC721TxBindings, ERC721LogBindings> {
   public readonly entity = 'token';
   public readonly standard = 'ERC721';
-  public readonly decoder = createAbiDecoder(__dirname + '../../../abi/erc721.json');
+  public readonly abiDecoder = createAbiDecoder(__dirname + '../../../abi/erc721.json');
 
-  public readonly transformers = {
+  public readonly txTransformers = {
     safeTransferFrom: (decoded: any, tx: EthqlTransaction, context: EthqlContext) => {
       const tokenContract = new ERC721TokenContract(tx.to, context);
       const from = new EthqlAccount(extractParamValue(decoded.params, '_from'));
@@ -124,6 +147,44 @@ class ERC721Token {
       };
     },
   };
+
+  public readonly logTransformers = {
+    transfer: (decoded: any, tx: EthqlTransaction, context: EthqlContext): ERC721TransferEvent => {
+      const tokenContract = new ERC721TokenContract(tx.to, context);
+      const from = new EthqlAccount(extractParamValue(decoded.events, '_from'));
+      const to = new EthqlAccount(extractParamValue(decoded.events, '_to'));
+
+      return {
+        from: new ERC721TokenHolder(from, tokenContract),
+        to: new ERC721TokenHolder(to, tokenContract),
+        tokenID: extractParamValue(decoded.events, '_tokenId'),
+      };
+    },
+
+    approval: (decoded: any, tx: EthqlTransaction, context: EthqlContext): ERC721ApprovalEvent => {
+      const tokenContract = new ERC721TokenContract(tx.to, context);
+      const owner = new EthqlAccount(extractParamValue(decoded.events, '_owner'));
+      const approved = new EthqlAccount(extractParamValue(decoded.events, '_approved'));
+
+      return {
+        owner: new ERC721TokenHolder(owner, tokenContract),
+        approved: new ERC721TokenHolder(approved, tokenContract),
+        tokenID: extractParamValue(decoded.events, '_tokenId'),
+      };
+    },
+
+    approvalForAll: (decoded: any, tx: EthqlTransaction, context: EthqlContext): ERC721ApprovalForAllEvent => {
+      const tokenContract = new ERC721TokenContract(tx.to, context);
+      const owner = new EthqlAccount(extractParamValue(decoded.events, '_owner'));
+      const operator = new EthqlAccount(extractParamValue(decoded.events, '_operator'));
+
+      return {
+        owner: new ERC721TokenHolder(owner, tokenContract),
+        operator: new ERC721TokenHolder(operator, tokenContract),
+        approved: extractParamValue(decoded.events, '_tokenId'),
+      };
+    },
+  };
 }
 
-export default ERC721Token;
+export default ERC721TokenDecoder;
--- src/dec/index.ts
+++ src/dec/index.ts
@@ -1,4 +1,5 @@
 import Erc20TokenDecoder from './decoders/Erc20TokenDecoder';
+import ERC721TokenDecoder from './decoders/erc721TokenDecoder';
 import { SimpleDecodingEngine } from './engines/SimpleDecodingEngine';
 import { DecodingEngine } from './types';
 
@@ -7,6 +8,7 @@ const engine = new SimpleDecodingEngine();
 
 // Register all decoders.
 engine.register(new Erc20TokenDecoder());
+//engine.register(new ERC721TokenDecoder());
 
 // Export the engine, making only the decodeTransaction method visible.
 export default engine as Pick<DecodingEngine, 'decodeTransaction' | 'decodeLog'>;
